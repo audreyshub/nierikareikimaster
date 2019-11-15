241ebaa4d3c10124cdc8cf9b3fc6bb17d8adff1b
Audrey Wong <audreydotwong@gmail.com>
2019-11-15 09:38:58 -0500

    change message to field

--- mail.php
+++ mail.php
@@ -4,12 +4,13 @@ $name = $_POST['name'];
 if(isset( $_POST['email']))
 $email = $_POST['email'];
 if(isset( $_POST['message']))
-$message = $_POST['message'];
+//change message to field
+field = $_POST['field'];
 
 
-$content="From: $name \n Email: $email \n Message: $message";
+$content="From: $name \n Email: $email \n Message: field";
 $recipient = "audreydotwong@gmail.com";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
+mail($recipient, $content, $mailheader) or die("Error!");
 echo "Email sent!";
 ?>
