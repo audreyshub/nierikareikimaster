87d41f10f566cd2fa64fede0d61e86f22e432aac
Audrey Wong <audreydotwong@gmail.com>
2019-11-15 09:58:40 -0500

    fix mail.php

--- mail.php
+++ mail.php
@@ -8,7 +8,7 @@ if(isset( $_POST['field']))
 $field = $_POST['field'];
 
 
-$content="From: $name \n Email: $email \n Message: field";
+$content="From: $name \n Email: $email \n Message: $field";
 $recipient = "audreydotwong@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $content, $mailheader) or die("Error!");
