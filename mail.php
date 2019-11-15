88d50c52d5a798ae149401ceb6455271bb07c9c5
Audrey Wong <audreydotwong@gmail.com>
2019-11-15 11:05:42 -0500

    swap out mail.php

--- mail.php
+++ mail.php
@@ -1,26 +1,39 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-/*
-Tested working with PHP5.4 and above (including PHP 7 )
-
- */
-require_once './vendor/autoload.php';
-
-use FormGuide\Handlx\FormHandler;
-
-
-$pp = new FormHandler();
-
-$validator = $pp->getValidator();
-$validator->fields(['name','email'])->areRequired()->maxLength(50);
-$validator->field('email')->isEmail();
-$validator->field('field')->maxLength(6000);
-
-
-
-
-$pp->sendEmailTo('audreydotwong@gmail.com'); // ← Your email here
-
-echo $pp->process($_POST);
+if ($_POST["email"]<>'') {
+    $ToEmail = 'audreydotwong@gmail.com';
+    $EmailSubject = 'Site contact form';
+    $mailheader = "From: ".$_POST["email"]."\r\n";
+    $mailheader .= "Reply-To: ".$_POST["email"]."\r\n";
+    $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n";
+    $MESSAGE_BODY = "Name: ".$_POST["name"]."";
+    $MESSAGE_BODY .= "Email: ".$_POST["email"]."";
+    $MESSAGE_BODY .= "Comment: ".nl2br($_POST["field"])."";
+    mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure");
+?>
+Your message was sent
+<?php
+} else {
+?>
+<form action="test.php" method="post">
+<table width="400" border="0" cellspacing="2" cellpadding="0">
+<tr>
+<td width="29%" class="bodytext">Your name:</td>
+<td width="71%"><input name="name" type="text" id="name" size="32"></td>
+</tr>
+<tr>
+<td class="bodytext">Email address:</td>
+<td><input name="email" type="text" id="email" size="32"></td>
+</tr>
+<tr>
+<td class="bodytext">Comment:</td>
+<td><textarea name="comment" cols="45" rows="6" id="comment" class="bodytext"></textarea></td>
+</tr>
+<tr>
+<td class="bodytext"> </td>
+<td align="left" valign="top"><input type="submit" name="Submit" value="Send"></td>
+</tr>
+</table>
+</form>
+<?php
+};
+?>
